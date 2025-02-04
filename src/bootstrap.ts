67304a99784ff1fd45096c0016e465f1d73c3178
Omar Altamimi <ads.alsaheb@gmail.com>
2025-02-04 17:45:26 +0300

    the app is running

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,7 +1,7 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app/app.module';
+import { UserModule } from './app/app.module';
 
-platformBrowserDynamic().bootstrapModule(AppModule, {
+platformBrowserDynamic().bootstrapModule(UserModule, {
   ngZoneEventCoalescing: true,
 })
   .catch(err => console.error(err));
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,10 @@
 const { shareAll, withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
 
 module.exports = withModuleFederationPlugin({
-
-  name: 'mfe-user',
-
+  name: "mfeUser",
+  filename: "remoteEntry.js",
   exposes: {
-    './Component': './src/app/app.component.ts',
+    './UserModule': '../mfe-user/src/app/app.module.ts',
   },
-
-  shared: {
-    ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
-  },
-
+  shared: shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
 });
